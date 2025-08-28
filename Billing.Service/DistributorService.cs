b2ad271813030de204f73e87fee11cd2466fb6d7
Vinicius Nunes <vinicius.nunes81@gmail.com>
2025-08-28 13:39:14 -0300

    AddFluentValidation

--- Billing.Service/DistributorService.cs
+++ Billing.Service/DistributorService.cs
@@ -58,7 +58,7 @@ namespace MoveEnergia.Billing.Service
         }
         public async Task<ReturnResponseDto> UpdateAsync(Distributor distributor)
         {
-            var validation = await Validation.ValidateAsync(distributor, _validator);
+            var validation = await Validation.ProcessValidateAsync(distributor, _validator);
 
             if (validation != null && validation.Count > 0)
             {
